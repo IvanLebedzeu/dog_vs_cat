9e7411459e4aa6cf1305f0d1e9bae46f7981f1c2
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-03-24 19:19:31 -0700

    it will show how I add imgs before and after elements and the remove them

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,37 @@ $(document).ready(function() {
     $("ul#dog").prepend("<li>Bark!</li>");
   });
 
+  $("h1").click(function() {
+   // $("ul#cat").prepend("<li>Meow!</li>");
+   //  $("ul#dog").prepend("<li>Bark!</li>");
+   $("h1").before("<img src='https://i.ytimg.com/vi/C-sWokqiVHw/maxresdefault.jpg'>");
+   //$("p").after("<img src='https://vestikamaza.ru/uploads/vestikamaza/blog/illustration/image/a9bb8b/preview_8ydrnf45rgy.jpg'>");
+   $("img").click(function() {
+    $("img").remove();
+  });
+  });
+
+  $("p").click(function() {
+    // $("ul#cat").prepend("<li>Meow!</li>");
+    //  $("ul#dog").prepend("<li>Bark!</li>");
+    //$("h1").before("<img src='https://i.ytimg.com/vi/C-sWokqiVHw/maxresdefault.jpg'>");
+    $("p").after("<div id = 'div1'><img src='https://vestikamaza.ru/uploads/vestikamaza/blog/illustration/image/a9bb8b/preview_8ydrnf45rgy.jpg'></div>");
+    $("#div1").click(function() {
+      $("#div1").remove();
+    });
+   });
+
+   
+
+  // $("h1").children("img").first().click(function() {
+    //$(this).remove();
+  //});
+  //$("p").children("img").first().click(function() {
+    //$(this).remove();
+  //});
+
+
+  
 });
 
 
